585988f694d7cab8e0803f64f089f90d065b40b2
Kjartan Hrafnkelsson <kjartan@kjartan.io>
2022-10-30 21:37:56 +0000

    open pdb link in new tab
    @Ewpratten also give opinion here  💯

--- scripts/peering-db-data.js
+++ scripts/peering-db-data.js
@@ -16,8 +16,8 @@ function get_top_tabs() {
 }
 
 // Add an item to the top tab bar
-function add_top_tab_item(name, url) {
-    get_top_tabs().innerHTML += `<li class="custom-top-tab-item"><a href="${url}">${name}</a></li>`;
+function add_top_tab_item(name, url, newtab = false) {
+    get_top_tabs().innerHTML += `<li class="custom-top-tab-item"><a href="${url}"${newtab ? `target="_blank" rel="noreferrer"` : ''}>${name}</a></li>`;
 }
 
 // Get the page id (This is an ASN or prefix)
@@ -46,7 +46,7 @@ function get_page_id() {
     `;
 
     // Add PeeringDB button
-    add_top_tab_item(`<img src="https://www.google.com/s2/favicons?sz=64&domain=peeringdb.com" style="width:16px;">`, `https://peeringdb.com/asn/${get_page_id()}`);
+    add_top_tab_item(`<img src="https://www.google.com/s2/favicons?sz=64&domain=peeringdb.com" style="width:16px;">`, `https://peeringdb.com/asn/${get_page_id()}`, true);
 
     // Handle adding a domain query button
     var asn_info_text = document.getElementById("network-number").innerText;
