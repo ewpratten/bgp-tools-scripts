34f062b1fb9cafdfa727c1f0dfaea2a2e848995d
Evan Pratten <ewpratten@gmail.com>
2022-10-30 17:29:42 -0400

    nlnog lg

---
+++ scripts/nlnog-ring-lg.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         BGP.tools NLNOG Looking Glass button
+// @namespace    https://ewpratten.com
+// @version      1.0
+// @description  NLNOG Looking Glass button for BGP.tools
+// @author       Evan Pratten <ewpratten@gmail.com>
+// @match        https://bgp.tools/prefix/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bgp.tools
+// ==/UserScript==
+
+
+// Gets the ul object controlling the top tabs
+function get_top_tabs() {
+    return document.getElementsByClassName("section-tabs")[0].getElementsByTagName("ul")[0];
+}
+
+// Add an item to the top tab bar
+function add_top_tab_item(name, url) {
+    get_top_tabs().innerHTML += `<li class="custom-top-tab-item"><a href="${url}">${name}</a></li>`;
+}
+
+// Get the page id (This is an ASN or prefix)
+function get_page_id() {
+    var path_parts = document.location.pathname.split("/");
+    if (path_parts.length == 3) { return path_parts[2]; }
+    if (path_parts.length >= 4) { return path_parts[2] + "/" + path_parts[3]; }
+    return null;
+}
+// Entry
+(function() {
+    'use strict';
+    
+    // Get the prefix
+    var prefix = get_page_id();
+    var prefix_urlencode = encodeURI(prefix);
+
+    // Add NLNOG button
+    add_top_tab_item("NLNOG LG", `https://lg.ring.nlnog.net/prefix?q=${prefix_urlencode}&match=exact&peer=all`);
+})();
--- scripts/peering-db-data.js
+++ scripts/peering-db-data.js
@@ -4,7 +4,7 @@
 // @version      1.0
 // @description  PeeringDB integration for BGP.tools
 // @author       Evan Pratten <ewpratten@gmail.com>
-// @match        https://bgp.tools/*
+// @match        https://bgp.tools/as/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bgp.tools
 // ==/UserScript==
 
@@ -20,13 +20,6 @@ function add_top_tab_item(name, url) {
     get_top_tabs().innerHTML += `<li class="custom-top-tab-item"><a href="${url}">${name}</a></li>`;
 }
 
-// Get the page type
-function get_page_type() {
-    var path_parts = document.location.pathname.split("/");
-    if (path_parts.length > 1) { return path_parts[1]; }
-    return null;
-}
-
 // Get the page id (This is an ASN or prefix)
 function get_page_id() {
     var path_parts = document.location.pathname.split("/");
@@ -36,7 +29,7 @@ function get_page_id() {
 }
 
 // Entry
-(function() {
+(function () {
     'use strict';
 
     // Inject custom styles
@@ -52,95 +45,93 @@ function get_page_id() {
     crossorigin="anonymous" referrerpolicy="no-referrer" />
     `;
 
-    // Handle per-page changes
-    if (get_page_type() == "as") {
-        // Add PeeringDB button
-        add_top_tab_item(`<img src="https://www.google.com/s2/favicons?sz=64&domain=peeringdb.com" style="width:16px;">`, `https://peeringdb.com/asn/${get_page_id()}`);
+    // Add PeeringDB button
+    add_top_tab_item(`<img src="https://www.google.com/s2/favicons?sz=64&domain=peeringdb.com" style="width:16px;">`, `https://peeringdb.com/asn/${get_page_id()}`);
 
-        // Handle adding a domain query button
-        var asn_info_text = document.getElementById("network-number").innerText;
-        if (asn_info_text.includes("Website")) {
-            console.log("Attempting to patch domain query button");
-            var url = asn_info_text.match(/^Website (?<url>.*)$/m).groups.url;
-            var domain = (new URL(url)).hostname;
-            document.getElementById("network-number").innerHTML += ` <strong>(<a href="/dns/${domain}">Query</a>)</strong>`;
-        }
-
-        // Move the network name to above the image and info
-        document.getElementById("network-name").parentElement.parentElement.prepend(document.getElementById("network-name"));
-        document.getElementsByClassName("network-header")[0].prepend(document.createElement("br"));
-        document.getElementsByClassName("network-header")[0].prepend(document.createElement("hr"));
-
-        // In-fill data from PeeringDB
-        fetch(
-            `https://www.peeringdb.com/api/net?asn=${get_page_id()}`,
-            {
-                headers: {
-                    'Authorization': `Api-Key ${PEERINGDB_API_KEY}`
-                }
-            }
-        ).then(
-            resp => resp.json().then(
-                data => {
-                    // Skip if there is no result for this asn
-                    if (data.data.length == 0) { return; }
-                    var asn_info = data.data[0];
-                    console.log(asn_info);
-
-                    // Substitute the network name
-                    var existing_name = document.getElementById("network-name").innerText;
-                    if (existing_name != asn_info.name) {
-                        document.getElementById("network-name").innerText = asn_info.name;
-                        document.getElementById("network-name").title = `Community submission: ${existing_name}`;
-                        document.getElementById("network-name").style.borderBottom = "3px dashed #e3e3e3";
-                        document.getElementById("network-name").style.width = "max-content";
-                        document.getElementById("network-name").style.cursor = "help";
-                        document.head.getElementsByTagName("title")[0].innerText = `AS${get_page_id()} ${asn_info.name} - bgp.tools`;
-                    }
-
-                    // Add extra info from PDB
-                    if (asn_info.info_traffic) {
-                        document.getElementById("network-number").innerHTML += `<br>Traffic <strong>${asn_info.info_traffic}</strong>`;
-                        if (asn_info.info_ratio) {
-                        document.getElementById("network-number").innerHTML += ` <strong>(${asn_info.info_ratio})</strong>`;
-                        }
-                    }
-                    if (asn_info.policy_general) {
-                        document.getElementById("network-number").innerHTML += `<br>Peering <strong>${asn_info.policy_general}</strong>`;
-                    }
-                    if (asn_info.irr_as_set) {
-                        document.getElementById("network-number").innerHTML += `<br>Default as-set <strong><a href="https://irrexplorer.nlnog.net/as-set/${asn_info.irr_as_set}">${asn_info.irr_as_set}</a></strong>`;
-                    }
-
-                    // Add network link buttons
-                    if (asn_info.looking_glass) {
-                        add_top_tab_item(`<i class="fa-solid fa-magnifying-glass"></i>`, asn_info.looking_glass);
-                    }
-                    if (asn_info.route_server) {
-                        add_top_tab_item(`<i class="fa-solid fa-server"></i>`, asn_info.route_server);
-                    }
-                    if (asn_info.status_dashboard) {
-                        add_top_tab_item(`<i class="fa-solid fa-circle-info"></i>`, asn_info.status_dashboard);
-                    }
-
-                    // Add even more data using the org info
-                    fetch(
-                        `https://www.peeringdb.com/api/org/${asn_info.org_id}`,
-                        {
-                            headers: {
-                                'Authorization': `Api-Key ${PEERINGDB_API_KEY}`
-                            }
-                        }
-                    ).then(
-                        resp => resp.json().then(
-                            data => {
-                                document.getElementById("network-number").innerHTML += `<br>Operated by <strong>${data.data[0].name}</strong`;
-
-                            }
-                        )
-                    )
-                }
-            )
-        );
+    // Handle adding a domain query button
+    var asn_info_text = document.getElementById("network-number").innerText;
+    if (asn_info_text.includes("Website")) {
+        console.log("Attempting to patch domain query button");
+        var url = asn_info_text.match(/^Website (?<url>.*)$/m).groups.url;
+        var domain = (new URL(url)).hostname;
+        document.getElementById("network-number").innerHTML += ` <strong>(<a href="/dns/${domain}">Query</a>)</strong>`;
     }
+
+    // Move the network name to above the image and info
+    document.getElementById("network-name").parentElement.parentElement.prepend(document.getElementById("network-name"));
+    document.getElementsByClassName("network-header")[0].prepend(document.createElement("br"));
+    document.getElementsByClassName("network-header")[0].prepend(document.createElement("hr"));
+
+    // In-fill data from PeeringDB
+    fetch(
+        `https://www.peeringdb.com/api/net?asn=${get_page_id()}`,
+        {
+            headers: {
+                'Authorization': `Api-Key ${PEERINGDB_API_KEY}`
+            }
+        }
+    ).then(
+        resp => resp.json().then(
+            data => {
+                // Skip if there is no result for this asn
+                if (data.data.length == 0) { return; }
+                var asn_info = data.data[0];
+                console.log(asn_info);
+
+                // Substitute the network name
+                var existing_name = document.getElementById("network-name").innerText;
+                if (existing_name != asn_info.name) {
+                    document.getElementById("network-name").innerText = asn_info.name;
+                    document.getElementById("network-name").title = `Community submission: ${existing_name}`;
+                    document.getElementById("network-name").style.borderBottom = "3px dashed #e3e3e3";
+                    document.getElementById("network-name").style.width = "max-content";
+                    document.getElementById("network-name").style.cursor = "help";
+                    document.head.getElementsByTagName("title")[0].innerText = `AS${get_page_id()} ${asn_info.name} - bgp.tools`;
+                }
+
+                // Add extra info from PDB
+                if (asn_info.info_traffic) {
+                    document.getElementById("network-number").innerHTML += `<br>Traffic <strong>${asn_info.info_traffic}</strong>`;
+                    if (asn_info.info_ratio) {
+                        document.getElementById("network-number").innerHTML += ` <strong>(${asn_info.info_ratio})</strong>`;
+                    }
+                }
+                if (asn_info.policy_general) {
+                    document.getElementById("network-number").innerHTML += `<br>Peering <strong>${asn_info.policy_general}</strong>`;
+                }
+                if (asn_info.irr_as_set) {
+                    document.getElementById("network-number").innerHTML += `<br>Default as-set <strong><a href="https://irrexplorer.nlnog.net/as-set/${asn_info.irr_as_set}">${asn_info.irr_as_set}</a></strong>`;
+                }
+
+                // Add network link buttons
+                if (asn_info.looking_glass) {
+                    add_top_tab_item(`<i class="fa-solid fa-magnifying-glass"></i>`, asn_info.looking_glass);
+                }
+                if (asn_info.route_server) {
+                    add_top_tab_item(`<i class="fa-solid fa-server"></i>`, asn_info.route_server);
+                }
+                if (asn_info.status_dashboard) {
+                    add_top_tab_item(`<i class="fa-solid fa-circle-info"></i>`, asn_info.status_dashboard);
+                }
+
+                // Add even more data using the org info
+                fetch(
+                    `https://www.peeringdb.com/api/org/${asn_info.org_id}`,
+                    {
+                        headers: {
+                            'Authorization': `Api-Key ${PEERINGDB_API_KEY}`
+                        }
+                    }
+                ).then(
+                    resp => resp.json().then(
+                        data => {
+                            document.getElementById("network-number").innerHTML += `<br>Operated by <strong>${data.data[0].name}</strong`;
+
+                        }
+                    )
+                )
+            }
+        )
+    );
+
 })();
