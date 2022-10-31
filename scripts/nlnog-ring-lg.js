74eeecae38642d7c8f14994f5fd3c6bb2fdb8b70
Evan Pratten <ewpratten@gmail.com>
2022-10-31 19:21:04 -0400

    Add common utils

--- scripts/nlnog-ring-lg.js
+++ scripts/nlnog-ring-lg.js
@@ -6,26 +6,9 @@
 // @author       Evan Pratten <ewpratten@gmail.com>
 // @match        https://bgp.tools/prefix/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bgp.tools
+// @requires     https://raw.githubusercontent.com/Ewpratten/bgp-tools-scripts/master/scripts/utils.js
 // ==/UserScript==
 
-
-// Gets the ul object controlling the top tabs
-function get_top_tabs() {
-    return document.getElementsByClassName("section-tabs")[0].getElementsByTagName("ul")[0];
-}
-
-// Add an item to the top tab bar
-function add_top_tab_item(name, url) {
-    get_top_tabs().innerHTML += `<li class="custom-top-tab-item"><a href="${url}">${name}</a></li>`;
-}
-
-// Get the page id (This is an ASN or prefix)
-function get_page_id() {
-    var path_parts = document.location.pathname.split("/");
-    if (path_parts.length == 3) { return path_parts[2]; }
-    if (path_parts.length >= 4) { return path_parts[2] + "/" + path_parts[3]; }
-    return null;
-}
 // Entry
 (function() {
     'use strict';
--- scripts/peering-db-data.js
+++ scripts/peering-db-data.js
@@ -6,27 +6,10 @@
 // @author       Evan Pratten <ewpratten@gmail.com>
 // @match        https://bgp.tools/as/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bgp.tools
+// @requires     https://raw.githubusercontent.com/Ewpratten/bgp-tools-scripts/master/scripts/utils.js
 // ==/UserScript==
 
 const PEERINGDB_API_KEY = "<your_api_key>";
-
-// Gets the ul object controlling the top tabs
-function get_top_tabs() {
-    return document.getElementsByClassName("section-tabs")[0].getElementsByTagName("ul")[0];
-}
-
-// Add an item to the top tab bar
-function add_top_tab_item(name, url, newtab = false) {
-    get_top_tabs().innerHTML += `<li class="custom-top-tab-item"><a href="${url}"${newtab ? `target="_blank" rel="noreferrer"` : ''}>${name}</a></li>`;
-}
-
-// Get the page id (This is an ASN or prefix)
-function get_page_id() {
-    var path_parts = document.location.pathname.split("/");
-    if (path_parts.length == 3) { return path_parts[2]; }
-    if (path_parts.length >= 4) { return path_parts[2] + "/" + path_parts[3]; }
-    return null;
-}
 
 // Entry
 (function () {
