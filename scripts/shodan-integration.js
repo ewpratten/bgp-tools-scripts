aa986c39ef402660f036b514a91d7df1993119bb
Evan Pratten <ewpratten@gmail.com>
2022-10-31 19:56:53 -0400

    Add a shodan script

---
+++ scripts/shodan-integration.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         BGP.tools - Shodan integration
+// @namespace    https://ewpratten.com
+// @version      1.0
+// @description  Shodan integration for BGP.tools
+// @author       Evan Pratten <ewpratten@gmail.com>
+// @match        https://bgp.tools/as/*
+// @match        https://bgp.tools/prefix/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bgp.tools
+// @require      https://raw.githubusercontent.com/Ewpratten/bgp-tools-scripts/master/scripts/utils.js
+// ==/UserScript==
+
+// Entry
+(function () {
+    'use strict';
+
+    // Per-page changes
+    switch (get_page_type()) {
+        case "as":
+            add_top_tab_item(`<img src="https://www.google.com/s2/favicons?sz=64&domain=shodan.io" style="width:16px;">`, `https://www.shodan.io/search?query=asn:${get_page_id()}`);
+            break;
+        case "prefix":
+            var prefix_urlencode = encodeURI(get_page_id());
+            add_top_tab_item(`<img src="https://www.google.com/s2/favicons?sz=64&domain=shodan.io" style="width:16px;">`, `https://www.shodan.io/search?query=net%3A%22${prefix_urlencode}%22`);
+            break;
+    }
+
+})();
