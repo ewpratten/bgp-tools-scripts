a1a41e860206c0d8b81f45bdeb7733b4d886c2be
Quantum <quantum2048@gmail.com>
2023-09-18 22:21:31 -0400

    Refactor ixp-peering-capacity.js to be more maintainable
    Also added the ability to do automatic updates.

--- scripts/ixp-peering-capacity.js
+++ scripts/ixp-peering-capacity.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name         BGP.tools Peering Capacity Display
 // @namespace    http://kjartan.io/
-// @version      0.1
+// @version      0.2
+// @updateURL    https://raw.githubusercontent.com/ewpratten/bgp-tools-scripts/master/scripts/ixp-peering-capacity.js
+// @downloadURL  https://raw.githubusercontent.com/ewpratten/bgp-tools-scripts/master/scripts/ixp-peering-capacity.js
 // @description  Displays the total IX peering capacity of any given network on BGP.tools
 // @author       Kjartan Hrafnkelsson
 // @match        https://bgp.tools/as/*
@@ -9,34 +11,45 @@
 // @grant        none
 // ==/UserScript==
 
-function formatBytes(bytes, decimals = 2) {
-    if (!+bytes) return '0 Bytes'
-
-    const k = 1000
-    const dm = decimals < 0 ? 0 : decimals
-    const sizes = ['bps', 'kbps', 'mbps', 'gbps', 'tbps', 'pbps', 'ebps', 'zbps', 'ybps']
-
-    const i = Math.floor(Math.log(bytes) / Math.log(k))
-
-    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+function formatBps(bps, decimals = 2) {
+  const sizes = ['bps', 'kbps', 'Mbps', 'Gbps', 'Tbps', 'Pbps', 'Ebps', 'Zbps', 'Ybps'];
+  const i = Math.floor(Math.log(+bps) / Math.log(1000));
+  return `${((+bps / Math.pow(1000, i)).toFixed(Math.max(0, decimals)))} ${sizes[i]}`;
 }
 
-(function() {
-    'use strict';
-	let _s = [];
-	let _c = 0;
-	document.querySelector('#ix-page').querySelectorAll('table').forEach(e => e.querySelectorAll('tr').forEach(b => b.querySelectorAll('td')[4] !== undefined ? _s.push(b.querySelectorAll('td')[4].textContent) : null));
-	_s.forEach(e => {
-		let _e = e.split(' ')
-		if (_e[1] === "mbps") _c = _c + parseInt(_e[0])
-		else if (_e[1] === "gbps") _c = _c + parseInt(_e[0]) * 1000
-		else if (_e[1] === "tbps") _c = _c + parseInt(_e[0]) * 1000000
-	});
-	let _f = formatBytes(_c * 1000000);
-	if (document.querySelector('#peering-capacity')) document.querySelector('#peering-capacity').innerHTML = _f
-	else {
-		document.querySelector('#network-number').style.marginBottom = "0px";
-		document.querySelector('.network-header').innerHTML = document.querySelector('.network-header').innerHTML + "<p style='margin-top:0;'>IX Peering Capacity <strong id='peering-capacity'>" + _f + "</strong></p>";
-	}
-        console.log('[ixp-capacity-plugin] calculated peering capacity (inital page load).');
+function parseBps(text) {
+  const [s, u] = text.split(' ');
+  const unit = u.toLowerCase();
+  const sizes = ['bps', 'kbps', 'mbps', 'gbps', 'tbps', 'pbps', 'ebps', 'zbps', 'ybps'];
+  if (!sizes.includes(unit) || !+s) return 0;
+  return s * Math.pow(1000, sizes.indexOf(unit));
+}
+
+function getOrCreateField() {
+  const field = document.querySelector('#peering-capacity');
+  if (field) return field;
+
+  const p = document.createElement('p');
+  p.style.marginTop = 0;
+  p.innerText = 'IX Peering Capacity ';
+
+  const strong = document.createElement('strong');
+  strong.id = 'peering-capacity';
+  p.appendChild(strong);
+
+  document.querySelector('#network-number').style.marginBottom = 0;
+  document.querySelector('.network-header').appendChild(p);
+  return strong;
+}
+
+(function () {
+  let capacity = 0;
+  document.querySelector('#ix-page').querySelectorAll('table tr').forEach(e => {
+    const speed = e.querySelectorAll('td')[4];
+    if (speed)
+      capacity += parseBps(speed.textContent);
+  });
+  getOrCreateField().innerText = formatBps(capacity);
+
+  console.log('[ixp-capacity-plugin] calculated peering capacity (inital page load).');
 })();
